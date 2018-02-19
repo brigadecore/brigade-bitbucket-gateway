db47c87a3433311bf4b36bc89035c351fd2eb94a
lukepatrick <lukephilips@gmail.com>
2018-02-19 16:00:37 -0700

    feat(bitbucket-gateway) simple repo:push event handling

--- brigade-bitbucket-gateway/cmd/brigade-bitbucket-gateway/server.go
+++ brigade-bitbucket-gateway/cmd/brigade-bitbucket-gateway/server.go
@@ -92,96 +92,20 @@ func HandleMultiple(payload interface{}, header webhooks.Header) {
 	store := kube.New(clientset, namespace)
 	store.GetProjects()
 
-	glhandler := whbitbucket.NewbitbucketHandler(store)
+	glhandler := whbitbucket.NewBitbucketHandler(store)
 
 	var repo, commit, secret string
-	secret = strings.Join(header["X-bitbucket-Token"], "")
+	secret = strings.Join(header["X-Hook-UUID"], "")
 
 	switch payload.(type) {
-	case bitbucket.PushEventPayload:
-		log.Println("case bitbucket.PushEventPayload")
-		release := payload.(bitbucket.PushEventPayload)
+	case bitbucket.RepoPushPayload:
+		log.Println("case bitbucket.RepoPushPayload")
+		release := payload.(bitbucket.RepoPushPayload)
 
-		repo = release.Project.PathWithNamespace
-		commit = release.CheckoutSHA
+		repo = release.Repository.FullName
+		commit = release.Push.Changes[0].Commits[0].Hash
 
 		glhandler.HandleEvent(repo, "push", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.TagEventPayload:
-		log.Println("case bitbucket.TagEventPayload")
-		release := payload.(bitbucket.TagEventPayload)
-
-		repo = release.Project.PathWithNamespace
-		commit = release.CheckoutSHA
-
-		glhandler.HandleEvent(repo, "tag", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.IssueEventPayload:
-		log.Println("case bitbucket.IssueEventPayload")
-		release := payload.(bitbucket.IssueEventPayload)
-
-		repo = release.Project.PathWithNamespace
-		commit = release.Project.DefaultBranch
-
-		glhandler.HandleEvent(repo, "issue", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.ConfidentialIssueEventPayload:
-		log.Println("case bitbucket.ConfidentialIssueEventPayload")
-		release := payload.(bitbucket.ConfidentialIssueEventPayload)
-
-		repo = release.Project.PathWithNamespace
-		commit = release.Project.DefaultBranch
-
-		glhandler.HandleEvent(repo, "issue", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.CommentEventPayload:
-		log.Println("case bitbucket.CommentEventPayload")
-		release := payload.(bitbucket.CommentEventPayload)
-
-		repo = release.Project.PathWithNamespace
-		commit = release.Commit.ID
-
-		glhandler.HandleEvent(repo, "comment", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.MergeRequestEventPayload:
-		log.Println("case bitbucket.MergeRequestEventPayload")
-		//release := payload.(bitbucket.MergeRequestEventPayload)
-
-		log.Println("do nothing")
-		// do nothing, waiting on
-		// https://github.com/go-playground/webhooks/pull/24
-		// repo = release.
-		// commit = release.Commit.ID
-
-		// //repo string, eventType string, commit string, payload []byte, secret)
-		// glhandler.HandleEvent(repo, "mergerequest", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.WikiPageEventPayload:
-		log.Println("case bitbucket.WikiPageEventPayload")
-		release := payload.(bitbucket.WikiPageEventPayload)
-
-		repo = release.Project.PathWithNamespace
-		commit = release.Project.DefaultBranch
-
-		glhandler.HandleEvent(repo, "wikipage", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.PipelineEventPayload:
-		log.Println("case bitbucket.PipelineEventPayload")
-		release := payload.(bitbucket.PipelineEventPayload)
-
-		repo = release.Project.PathWithNamespace
-		commit = release.Commit.ID
-
-		glhandler.HandleEvent(repo, "pipeline", commit, []byte(fmt.Sprintf("%v", release)), secret)
-
-	case bitbucket.BuildEventPayload:
-		log.Println("case bitbucket.BuildEventPayload")
-		release := payload.(bitbucket.BuildEventPayload)
-
-		repo = release.ProjectName
-		commit = release.SHA
-
-		glhandler.HandleEvent(repo, "build", commit, []byte(fmt.Sprintf("%v", release)), secret)
 
 	default:
 		log.Printf("Unsupported event")
